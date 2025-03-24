4d33a50ad488d25c64d3900d4867fc86ae6f9481
meshnehemia <meshnehemia7@gmail.com>
2025-03-24 18:53:57 +0300

    this is the updates

---
+++ week2/python/datastructures.py
@@ -0,0 +1,15 @@
+#tuples 
+tuples  = ('number', 'values', 'numbers','testing')
+print(tuples)
+print(tuples[2])
+print(tuples[0:2])
+
+#dictionaries 
+dict  = {"kenya": "nairobi", "uganda":"Kambaa","nigeria":"Abuja"}
+print(dict)
+print(dict["kenya"])
+
+
+#sets 
+set = {1,2,3,4,5,6,7,8,9,0}
+print(set.pop())
